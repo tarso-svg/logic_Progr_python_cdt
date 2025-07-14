ec5d376004e1407bb1f264991108be8af229e4a5
tarso em vocacao <rodriguesdosanjostarso@gmail.com>
2025-07-14 17:00:27 -0300

    primeira atividade de logica

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+#mvc
+#model
+
+
+#view 
+
+
+#controller
+
+
+
+def mostrar_menu():
+    print("/n---calculadora---")
+    print("1.adiçao ")
+    print("2.subtraçao")
+    print("5. sair")
+    print("---------")
+    
