906233d6957730dfc56bed18ef9c5e7f3c939a15
tarso em vocacao <rodriguesdosanjostarso@gmail.com>
2025-07-21 16:56:49 -0300

    atividade do dia.

---
+++ comparaçao.py
@@ -0,0 +1,45 @@
+###criando uma lista 
+## ivan, gustavo, rafael,  
+
+
+
+
+#qual seria o print e
+#input para repetir os nome?
+print("*** lista DE NOMES***")
+
+
+nomes =input("digite seu nome por nomes ").split(",")
+
+#nomes = [nome,stri() for nomes in nomes]
+
+#print(nome)
+
+print("\n quais operaçoes voces quer fazer.")
+print("1-adicionar nomes")
+print("2-remover nomes")
+print("3-lista de nomes")
+print("4-sair")
+
+
+while true:
+    opcao =input("n\digite a opçao desejada (1-4):")
+
+if opçao == "1":
+    #novo_nome =input("digite o nome a ser adicinado:")
+    #nomes.append(novo_nome)
+    #print(f"{nove_nome}foi adicionar a lista.")
+    print("foiadicionado a lista")
+
+    elif opçao == "1"
+
+
+
+
+
+
+
+
+
+
+ 
