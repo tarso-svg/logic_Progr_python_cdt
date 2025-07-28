0db1ef0b25af618c86d15997fb3a5b0f4f9048e2
tarso em vocacao <rodriguesdosanjostarso@gmail.com>
2025-07-28 18:34:19 -0300

    Atividade em sala

---
+++ desejos.py
@@ -0,0 +1,16 @@
+print("💙✨ minha lista de desejos para o futuro ✨💙\n")
+
+NOME_ARQUIVO = "meus desejos.txt"
+desejos = []
+
+try
+    with open(NOME ARQUIVO, 'r' encoding=utf-8) as arquivo: for linha in arquivo: 
+    print (f"meus desejos foram carregados do arquivo '{NOME_ARQUIVO}'\n!")
+    EXCEPT FILENOTFOUNDERROR:
+    print("parece que e sua primrira vez vamos criar sua list de desejos.\n")
+except exception as e:
+    print (f"ocorreu um erro ao carregar os desejos: {e}")
+    def salvar_desejos (lista_de_desejos):
+        with open (NOME_ARQUIVO, 'w' encoding= 'utf-8')as arquivo
+        for in lista__de_desejos
+        
